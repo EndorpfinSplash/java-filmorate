71f022d45703eaa4d0fc1f043b3a235f6ad5321a
ANDREY ZINOVICH <andrey.zinovich@consultant.turkcell.com.tr>
2024-01-25 17:20:17 +0300

    dev: basics objects

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,63 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.UserAlreadyExistException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+public class UserController {
+
+    private final HashMap<Integer, User> users = new HashMap<>();
+
+    @GetMapping
+    public List<User> findAll() {
+        return new ArrayList(users.values());
+    }
+
+    @GetMapping
+    public User getUser(int userId) {
+        return users.get(userId);
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+
+        String email = user.getEmail();
+
+        if (email == null || email.isBlank()) {
+            throw new ValidationException("Invalid email");
+        }
+
+        if (users.containsKey(email)) {
+            throw new UserAlreadyExistException("User already exists");
+        }
+
+        users.put(user.getId(), user);
+        log.info("User was created");
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+
+        String email = user.getEmail();
+
+        if (email == null || email.isBlank()) {
+            throw new ValidationException("Invalid email");
+        }
+
+        users.put(user.getId(), user);
+        log.info("User was updated");
+        return user;
+    }
+
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+/**
+ * Film.
+ */
+@Data
+public class User {
+    private int id;
+    @Email
+    private String email;
+    @NotNull
+    private String login;
+    private String name;
+    private LocalDate birthday;
+}
