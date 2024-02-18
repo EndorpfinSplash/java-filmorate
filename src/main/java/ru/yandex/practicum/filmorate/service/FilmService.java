b438fb8b52cafa7cf1296efe26e41520ee263faa
Andrey Zinovich <fast.optimus@gmail.com>
2024-02-18 23:24:43 +0300

    fix: get correct sorted top film

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -7,7 +7,11 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparingInt;
 
 @Service
 public class FilmService {
@@ -46,18 +50,16 @@ public class FilmService {
     }
 
     public Film deleteLike(Integer filmId, Integer userId) {
-        Film filmForLike = getFilm(filmId);
+        Film filmForLike = filmStorage.getFilm(filmId);
         userStorage.getUser(userId);
         filmForLike.getLikes().remove(userId);
         return filmForLike;
     }
 
     public List<Film> getTopFilms(Integer count) {
-        TreeSet<Film> filmRating = new TreeSet<>(Comparator.comparing(film -> film.getLikes().size(), (o1, o2) -> o2 - o1));
-        filmRating.addAll(filmStorage.getAll());
-        if (count > filmRating.size()) {
-            return new ArrayList<>(filmRating);
-        }
-        return new ArrayList<>(filmRating).subList(0, count);
+        return filmStorage.getAll().stream()
+                .sorted(comparingInt(film -> -1 * film.getLikes().size()))
+                .limit(count)
+                .collect(Collectors.toList());
     }
 }
